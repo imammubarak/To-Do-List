c85e8e5d4a7038bb52f7009e295c6ba50aab038b
imammubarak <imamganiyumubarak@gmail.com>
2025-04-26 06:43:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+function AddTask(){
+  if (inputBox.value === ''){
+    alert('You must input Something');
+  }
+  else {
+    let li = document.createElement('li');
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    inputBox.value = '';
+  }
+
+}
